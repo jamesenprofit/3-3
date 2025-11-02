019b8d8f50fdd0c11bc3bc0c6004658520c597a6
jamesenprofit <jamesenprofit@live.de>
2025-11-02 13:21:30 +0100

    Kap4 - 1

--- main.c
+++ main.c
@@ -3,8 +3,20 @@
 int main()
 
 {
+int a , b , z , y , x , t;
+printf("Erste Eingabe:");
+scanf("%i",&a);
+printf("Zweite Eingabe:");
+scanf("%i",&b);
+z = a + b;
+y = a - b;
+x = a * b;
+t = a / b;
+printf("%i + %i = %i\n",a,b,z);
+printf("%i - %i = %i\n",a,b,y);
+printf("%i * %i = %i\n",a,b,x);
+printf("%i / %i = %i\n",a,b,t);
 
-printf("Das ergebnis von 4 mal 7 ist %i\n",4*7);
 return 0;
 
 }

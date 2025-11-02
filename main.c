cee5cc9506ad57f9f4a85048aea4ebcb4ea54004
jamesenprofit <jamesenprofit@live.de>
2025-11-02 18:28:10 +0100

    test

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+
+{
+int feld=1;
+long long int reis=1
+printf("\nFeld                                      Reis\n");
+    while (feld<=64)
+    {
+        printf("%3i     %20llu\n",feld,reis);
+        feld++;
+        reis*=2
+    }   
+
+
+return 0;
+
+}
